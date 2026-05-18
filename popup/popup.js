1f64b98c0f288ff3953b5b1897688de5aa95302b
abhay0826R-dev <abhay0826r@gmail.com>
2026-05-18 17:14:59 +0530

    feat: add popup UI with blacklist input

--- popup/popup.js
+++ popup/popup.js
@@ -1,23 +1,5 @@
 
-function removeInternships() {
-    let removed_internships_array = []
-    let remove_internship_array = ["Across The Globe", "Zdminds", "She Can Foundation"]
-    console.log("theS")
-    
-    for (let i=0; i<50; i++)
-    {
-        let x = document.querySelector(".individual_internship")
-        console.log(x)
-        console.log(x.innerHTML)
-        if (x.innerHTML.includes(remove_internship_array)) {
-            removed_internships_array.push(x)
-            x.remove()
-            console.log("Got 1")
-        }
-        else {
-            console.log("Na")
-        }
-    }
+function addjob(event) {
+    event.preventDefault()
+    console.log("Works")
 }
-
-document.getElementById("removeInternshipBtn").addEventListener("click", removeInternships)
