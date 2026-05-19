4443adf776d368db736ef59d538ad024122d1c0a
abhay0826R-dev <abhay0826r@gmail.com>
2026-05-19 21:58:51 +0530

    feat: Active/Inactive toggle on navbar

--- popup/popup.js
+++ popup/popup.js
@@ -4,16 +4,37 @@ function addjob(event) {
     console.log("Works")
 }
 
+function extensionToggleOff() {
+    overlayWarning.classList.add("hidden")
+    activeText.innerText = "ACTIVE"
+
+    // change color
+    activeText.classList.remove("red-activeText")
+    activeText.classList.add("blue-activeText")
+}
+
+function extensionToggleOn() {
+    overlayWarning.classList.remove("hidden")
+    activeText.innerText = "INACTIVE"
+
+    // change color
+    activeText.classList.add("red-activeText")
+    activeText.classList.remove("blue-activeText")
+}
+
 const activeCheckbox = document.getElementById("activeCheckBox")
+const activeText = document.getElementById("active-state-text")
 const overlayWarning = document.getElementById("site-warning-overlay")
 const overlayWarningReason = document.getElementById("overlay-warning-reason")
 
 // overlay blur warning 
 activeCheckbox.addEventListener("change", function() {
+console.log(activeText)
+
     if (activeCheckbox.checked) {
-        overlayWarning.classList.add("hidden")
+        extensionToggleOff()
     } else {
-        overlayWarning.classList.remove("hidden")
+        extensionToggleOn()
     }
     overlayWarningReason.innerText = "Extension OFF!"
 })
