44710dd118b3809bf2d906d7b1c50eec982f1d3b
abhay0826R-dev <abhay0826r@gmail.com>
2026-05-19 10:47:40 +0530

    feat: toggle on/off

--- popup/popup.js
+++ popup/popup.js
@@ -4,3 +4,16 @@ function addjob(event) {
     console.log("Works")
 }
 
+const activeCheckbox = document.getElementById("activeCheckBox")
+const overlayWarning = document.getElementById("site-warning-overlay")
+const overlayWarningReason = document.getElementById("overlay-warning-reason")
+
+// overlay blur warning 
+activeCheckbox.addEventListener("change", function() {
+    if (activeCheckbox.checked) {
+        overlayWarning.classList.add("hidden")
+    } else {
+        overlayWarning.classList.remove("hidden")
+    }
+    overlayWarningReason.innerText = "Extension OFF!"
+})
